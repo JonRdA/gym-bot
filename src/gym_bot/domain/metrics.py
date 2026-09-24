c9ae57ba3105b6137b47810617941e02098cea8c
Jon Ruiz de Azua <ruizdeazua.jon@gmail.com>
2026-09-24 11:54:13 +0200

    Update training conf

--- src/gym_bot/domain/metrics.py
+++ src/gym_bot/domain/metrics.py
@@ -13,6 +13,7 @@ METRIC_REGISTRY: dict[str, MetricDefinition] = {
     "weight":      MetricDefinition("weight",      "kg",   float),
     "time":        MetricDefinition("time",        "s",    int),
     "time_min":    MetricDefinition("time",        "min",  int),
+    "elevation":   MetricDefinition("elevation",   "m",    int),
     "distance":    MetricDefinition("distance",    "m",    float),
     "thigh2floor": MetricDefinition("thigh2floor", "cm",   float),
     "knee2floor":  MetricDefinition("knee2floor",  "cm",   float),
